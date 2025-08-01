2aa8ab2eb2f44e4e6a08389202869538ac05b025
Tegiminis <dragonmaw@gmail.com>
2025-07-31 19:08:04 -0700

    added newtonian flight added space backdrop so you can actually see how fast you're going (thanks nasa)

--- actors/player/player.gd
+++ actors/player/player.gd
@@ -1,6 +1,10 @@
-extends CharacterBody2D
+extends RigidBody2D
 
-@export var speed = 1
+@export var acceleration = 200
+@export var brake = 1
+
+@onready var gui_speed = $Control/Speed
+@onready var gui_torque = $Control/Rotation
 
 func _input(event: InputEvent) -> void:
 	pass
@@ -9,4 +13,24 @@ func _process(delta: float) -> void:
 	pass
 
 func _physics_process(delta: float) -> void:
-	pass
+	
+	linear_damp = 0
+	
+	gui_speed.text = str(linear_velocity)
+	gui_torque.text = str(angular_velocity)
+	
+	var direction = position.angle()
+	
+	if Input.is_action_pressed("thrust"):
+		apply_force(-transform.y * acceleration)
+	if Input.is_action_pressed("reverse"):
+		apply_force(transform.y * acceleration)
+	if Input.is_action_pressed("brake"):
+		linear_damp = brake
+	
+	if Input.is_action_pressed("rotate_ccw"):
+		apply_torque(-100)
+		
+	if Input.is_action_pressed("rotate_cw"):
+		apply_torque(100)
+	
