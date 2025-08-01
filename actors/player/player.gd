28ef18523000652973fb5d7903129f4d2911da62
Tegiminis <dragonmaw@gmail.com>
2025-07-31 17:52:44 -0700

    player setup project input settings for player movement

---
+++ actors/player/player.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+@export var speed = 1
+
+func _input(event: InputEvent) -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	pass
